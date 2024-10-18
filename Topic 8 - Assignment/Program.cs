aa8618a16f33dadd36ec408a6b05f3548aecd108
Carsen Greenwood <362570749@GoTVDSB.ca>
2024-10-18 09:38:51 -0400

    Add project files.

---
+++ Topic 8 - Assignment/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Reading in Colors");
+Console.WriteLine();
+Random generator = new Random();
+List<string> colors = new List<string> { "Red", "Blue", "Yellow", "Green", "Orange" };
+Console.WriteLine(string.Join(", ", colors));
+Console.WriteLine();
+Console.WriteLine("Here is a random color:");
+Console.WriteLine(colors[generator.Next(colors.Count)]);
+Console.WriteLine();
+
+Console.WriteLine("Random Numbers");
+int numberCount, numberMax, numberMin;
+Console.WriteLine();
+List<int> numbers = new List<int>();
+Console.WriteLine("How many numbers do you need?");
+Int32.TryParse(Console.ReadLine(), out numberCount);
+Console.WriteLine("What is your maximum value?");
+Int32.TryParse(Console.ReadLine(), out numberMax);
+Console.WriteLine("What is your minimum value?");
+Int32.TryParse(Console.ReadLine(), out numberMin);
+for (int i = numberMin + 1; i < numberMax + 1; i++)
+    numbers.Add(generator.Next(numberCount + 1));
+Console.WriteLine(string.Join(", ", numbers));
