33ad44962212d7ef00c89343507d40c3833cbcd5
Carsen Greenwood <362570749@GoTVDSB.ca>
2024-10-21 09:38:47 -0400

    Working

--- Topic 8 - Assignment/Program.cs
+++ Topic 8 - Assignment/Program.cs
@@ -10,7 +10,7 @@ Console.WriteLine(colors[generator.Next(colors.Count)]);
 Console.WriteLine();
 
 Console.WriteLine("Random Numbers");
-int numberCount, numberMax, numberMin;
+int numberCount, numberMax, numberMin, numberAppearance, count;
 Console.WriteLine();
 List<int> numbers = new List<int>();
 Console.WriteLine("How many numbers do you need?");
@@ -19,6 +19,13 @@ Console.WriteLine("What is your maximum value?");
 Int32.TryParse(Console.ReadLine(), out numberMax);
 Console.WriteLine("What is your minimum value?");
 Int32.TryParse(Console.ReadLine(), out numberMin);
-for (int i = numberMin + 1; i < numberMax + 1; i++)
-    numbers.Add(generator.Next(numberCount + 1));
+
+numberMax += 1;
+
+for (int i = 0; i < numberCount; i++)
+    numbers.Add(generator.Next(numberMin, numberMax));
 Console.WriteLine(string.Join(", ", numbers));
+Console.WriteLine();
+
+Console.WriteLine("Pick a number within the range, then I can tell you how many times it appears");
+Int32.TryParse(Console.ReadLine(), out numberAppearance);
